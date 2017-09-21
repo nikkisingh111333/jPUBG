33390490a0f4d76887367ed9ac996e4b961c8cca
Łukasz <lukasz.p.gorny@gmail.com>
2017-09-21 10:41:28 +0200

    Version 1.3.6
    * Added support for modes solo FPP, duo FPP and squad FPP

--- src/main/java/pro/lukasgorny/enums/PUBGMode.java
+++ src/main/java/pro/lukasgorny/enums/PUBGMode.java
@@ -1,10 +1,37 @@
 package pro.lukasgorny.enums;
 
+import com.google.gson.annotations.SerializedName;
+
 /**
  * Created by Łukasz "Husar" Górny on 2017-06-29.
  */
 public enum PUBGMode {
-    solo,
-    duo,
-    squad
+
+    @SerializedName("solo")
+    solo("solo"),
+
+    @SerializedName("duo")
+    duo("duo"),
+
+    @SerializedName("squad")
+    squad("squad"),
+
+    @SerializedName("solo-fpp")
+    solo_fpp("solo-fpp"),
+
+    @SerializedName("duo-fpp")
+    duo_fpp("duo-fpp"),
+
+    @SerializedName("squad-fpp")
+    squad_fpp("squad-fpp");
+
+    PUBGMode(String modeName) {
+        this.modeName = modeName;
+    }
+
+    private String modeName;
+
+    public String getModeName() {
+        return modeName;
+    }
 }
